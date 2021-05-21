1418e63d43efee9a6116f7c681c63102fb5733da
unknown <you@example.com>
2021-05-21 10:28:49 +0800

    renova 项目开工

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Renova 可视化图表</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homeIndex',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+</style>
